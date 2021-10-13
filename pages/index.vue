7664dc6dc2448bf271363bdbb201d96f275215aa
mon_sat <tnk@daijiten.info>
2021-10-13 14:01:31 +0900

    fix: fetch API

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
-<template>
-  <Tutorial/>
-</template>
+<script setup>
 
-<script>
-export default {}
+const counter = ref(0)
+
+const inc = () => counter.value++
+
+const hello = ref('')
+
+onBeforeMount(async () => {
+  hello.value = await $fetch('/api/hello')
+})
 </script>
+
+<template>
+  <div>
+    <div class="p-4">
+      <h1>Hello, Bridge !</h1>
+      <h2>{{ counter }}</h2>
+      <button
+        class="py-1 px-4 rounded shadow hover:shadow-none border border-red-200 bg-red-100"
+        @click="inc"
+      >Inc</button>
+      <h3>{{ hello }}</h3>
+    </div>
+    <Tutorial/>
+  </div>
+</template>
---
+++ server/api/hello.ts
@@ -0,0 +1,3 @@
+import type { IncomingMessage, ServerResponse } from 'http'
+
+export default (req: IncomingMessage, res: ServerResponse) => 'Hello, Bridge on API'
